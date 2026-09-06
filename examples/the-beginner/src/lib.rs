58b7d3afca17ccb2edfb0f1334034dc11ab5637b
Hugo Svirak <hugo@hugosvirak.com>
2026-09-06 15:18:46 +0200

    Experimental changes

---
+++ examples/the-beginner/src/lib.rs
@@ -0,0 +1,15 @@
+wit_bindgen::generate!({
+    world: "app",
+    path: "wit"
+});
+
+#[allow(dead_code)]
+struct App;
+
+impl Guest for App {
+    #[allow(async_fn_in_trait)]
+    fn run() -> () {
+        let temp = temperature::temperature();
+        temperature::log(&format!("{temp}"));
+    }
+}

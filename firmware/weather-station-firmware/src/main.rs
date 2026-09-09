83e3c5db55dad48ab0de0b7d70880039882a329f
Magnus-github <magnusrubentibbe@MacNussBook-Pro-4.local>
2026-09-09 19:34:31 +0200

    Make the wheel go brrrrrrrrr

--- firmware/weather-station-firmware/src/main.rs
+++ firmware/weather-station-firmware/src/main.rs
@@ -5,7 +5,6 @@
 //! Once the wifi is connected, the hostname will be set to "foo"
 //! Try pinging it from your PC with `ping foo`
 
-
 #![allow(unknown_lints)]
 #![allow(unexpected_cfgs)]
 
@@ -18,154 +17,111 @@ fn main() -> anyhow::Result<()> {
 
 #[cfg(any(esp32h2, esp32h4, esp32p4))]
 fn main() -> anyhow::Result<()> {
-    panic!("ESP32-H2, ESP32-H4 and ESP32-P4 do not have a Wifi radio (but you could enable the esp-wifi-remote component to use them with a WiFi co-processor)");
+    panic!(
+        "ESP32-H2, ESP32-H4 and ESP32-P4 do not have a Wifi radio (but you could enable the esp-wifi-remote component to use them with a WiFi co-processor)"
+    );
 }
 
 #[cfg(not(any(esp32h2, esp32h4, esp32p4)))]
 mod example {
     use core::convert::TryInto;
-use dotenvy_macro::dotenv;
+    use dotenvy_macro::dotenv;
 
     use embedded_dht_rs::dht11::Dht11;
-use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration as WifiConfiguration};
-use std::sync::Mutex;
+    use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration as WifiConfiguration};
     use esp_idf_svc::hal::delay::{Delay, FreeRtos};
-use esp_idf_svc::hal::gpio::PinDriver;
-use esp_idf_svc::hal::peripherals::Peripherals;
+    use esp_idf_svc::hal::gpio::PinDriver;
+    use esp_idf_svc::hal::gpio::Pull;
+    use esp_idf_svc::hal::ledc::config::TimerConfig;
+    use esp_idf_svc::hal::ledc::{LedcDriver, LedcTimerDriver};
+    use esp_idf_svc::hal::peripherals::Peripherals;
+    use esp_idf_svc::hal::units::*;
     use esp_idf_svc::http::Method;
-use esp_idf_svc::http::server::EspHttpServer;
-use esp_idf_svc::io::Write;
-use esp_idf_svc::ipv4::{
+    use esp_idf_svc::http::server::EspHttpServer;
+    use esp_idf_svc::io::Write;
+    use esp_idf_svc::ipv4::{
         ClientConfiguration as IpClientConfiguration, Configuration as IpConfiguration,
         DHCPClientSettings,
     };
     use esp_idf_svc::log::EspLogger;
     use esp_idf_svc::mdns::EspMdns;
     use esp_idf_svc::netif::{EspNetif, NetifConfiguration, NetifStack};
-    use esp_idf_svc::sys::{esp_wifi_set_ps, wifi_ps_type_t_WIFI_PS_NONE};
+    use esp_idf_svc::sys::{esp_wifi_set_ps, ledc_get_freq, wifi_ps_type_t_WIFI_PS_NONE};
     use esp_idf_svc::wifi::{BlockingWifi, EspWifi, WifiDriver};
     use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
-    use esp_idf_svc::hal::gpio::Pull;
+    use std::sync::Mutex;
 
     use log::info;
 
     const SSID: &str = dotenv!("WIFI_NAME");
-    const PASSWORD: &str = dotenv!("WIFI_PASSWORD"); 
+    const PASSWORD: &str = dotenv!("WIFI_PASSWORD");
 
     pub fn main() -> anyhow::Result<()> {
         esp_idf_svc::sys::link_patches();
         EspLogger::initialize_default();
 
         let peripherals = Peripherals::take()?;
-        let sys_loop = EspSystemEventLoop::take()?;
-        let nvs = EspDefaultNvsPartition::take()?;
+        // GPIO21 = direction control
+        let mut dir_pin = PinDriver::output(peripherals.pins.gpio9)?;
 
-        let dht11_pin = PinDriver::input_output_od(peripherals.pins.gpio4, Pull::Floating)?;
-
-        // let dht11_pin =
-            // PinDriver::output_od(peripherals.pins.gpio4)?;``
-        let delay = Delay::new_default();
-
-
-        let wifi = WifiDriver::new(peripherals.modem, sys_loop.clone(), Some(nvs))?;
-        let wifi = configure_wifi(wifi)?;
-
-        let mut wifi = BlockingWifi::wrap(wifi, sys_loop)?;
-        connect_wifi(&mut wifi)?;
-
-        // let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
-
-        // let mut dht11 = Dht11::new(dht11_pin, delay);
-
-
-        let dht11 = Mutex::new(Dht11::new(dht11_pin, delay));
-
-        let server_config = esp_idf_svc::http::server::Configuration::default();
-        let mut server = EspHttpServer::new(&server_config)?;
-
-        let mut mdns = EspMdns::take()?;
-        mdns.set_hostname("weather-station")?;
-
-        server.fn_handler("/temperature", Method::Get, move |req| {
-            let mut dht11 = dht11.lock().unwrap();
-
-            match dht11.read() {
-                Ok(reading) => {
-                    let body = format!(
-                        r#"{{"temperature":{}}}"#,
-                        reading.temperature
-                    );
-
-                    req.into_ok_response()?
-                        .write_all(body.as_bytes())
-                        .map(|_| ())
-                }
-
-                Err(error) => {
-                    log::error!("DHT11 read error: {:?}", error);
-
-                    req.into_status_response(500)?
-                        .write_all(br#"{"error":"Failed to read temperature"}"#)
-                        .map(|_| ())
-                }
-            }
-        })?;
-
-        loop {
-            std::thread::park();  // maybe implement some proper shutdown handler? I.e. block until some signal?
-        }
-
-        // info!("Wifi Interface info: {ip_info:?}");
-
-        // unsafe { esp_wifi_set_ps(wifi_ps_type_t_WIFI_PS_NONE); }
-        // loop {
-        //     std::thread::sleep(core::time::Duration::from_secs(5));
-        // }
-
-        // let od_for_dht11 = OutputOpenDrain::new(io.pins.gpio4, Level::High, Pull::None);
-
-        Ok(())
-    }
-
-    fn configure_wifi(wifi: WifiDriver) -> anyhow::Result<EspWifi> {
-        let mut wifi = EspWifi::wrap_all(
-            wifi,
-            // Note that setting a custom hostname can be used with any network adapter, not just Wifi
-            // I.e. that would work with Eth as well, because DHCP is an L3 protocol
-            EspNetif::new_with_conf(&NetifConfiguration {
-                ip_configuration: Some(IpConfiguration::Client(IpClientConfiguration::DHCP(
-                    DHCPClientSettings {
-                        hostname: Some("foo".try_into().unwrap()),
-                    },
-                ))),
-                ..NetifConfiguration::wifi_default_client()
-            })?,
-            #[cfg(esp_idf_esp_wifi_softap_support)]
-            EspNetif::new(NetifStack::Ap)?,
+        // Configure LEDC timer.
+        //
+        // 20 kHz is a reasonable PWM frequency for a motor driver.
+        let timer_driver = LedcTimerDriver::new(
+            peripherals.ledc.timer0,
+            &TimerConfig::default().frequency(20.kHz().into()),
         )?;
 
-        let wifi_configuration = WifiConfiguration::Client(ClientConfiguration {
-            ssid: SSID.try_into().unwrap(),
-            bssid: None,
-            auth_method: AuthMethod::WPA2Personal,
-            password: PASSWORD.try_into().unwrap(),
-            channel: None,
-            ..Default::default()
-        });
-        wifi.set_configuration(&wifi_configuration)?;
+        // GPIO20 = PWM1
+        let mut pwm_driver = LedcDriver::new(
+            peripherals.ledc.channel0,
+            timer_driver,
+            peripherals.pins.gpio15,
+        )?;
 
-        Ok(wifi)
-    }
+        // Start stopped.
+        pwm_driver.set_duty(pwm_driver.get_max_duty())?;
 
-    fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()> {
-        wifi.start()?;
-        info!("Wifi started");
+        // 50% duty cycle
+        let half_duty = pwm_driver.get_max_duty() / 2;
 
-        wifi.connect()?;
-        info!("Wifi connected");
+        loop {
+            // ---------------------------------------------------------
+            // Direction 1
+            // ---------------------------------------------------------
+            dir_pin.set_low()?;
 
-        wifi.wait_netif_up()?;
-        info!("Wifi netif up");
+            log::info!("Motor direction 1, PWM = 50%");
+            pwm_driver.set_duty(half_duty)?;
+
+            // Run for 5 seconds
+            FreeRtos::delay_ms(5000);
+
+            // Stop motor
+            log::info!("Motor stopped");
+            pwm_driver.set_duty(0)?;
+
+            // Wait 1 second before changing direction
+            FreeRtos::delay_ms(1000);
+
+            // ---------------------------------------------------------
+            // Direction 2
+            // ---------------------------------------------------------
+            dir_pin.set_high()?;
+
+            log::info!("Motor direction 2, PWM = 50%");
+            pwm_driver.set_duty(half_duty)?;
+
+            // Run for 5 seconds
+            FreeRtos::delay_ms(5000);
+
+            // Stop motor
+            log::info!("Motor stopped");
+            pwm_driver.set_duty(0)?;
+
+            // Wait 1 second before changing direction
+            FreeRtos::delay_ms(1000);
+        }
 
         Ok(())
     }

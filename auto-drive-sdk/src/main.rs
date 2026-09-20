bddfcf3d35cebb44b3693f32d892e6b8d4ef99fa
Hugo Svirak <hugo@hugosvirak.com>
2026-09-20 18:55:00 +0200

    Slightly more proper setup

---
+++ auto-drive-sdk/src/main.rs
@@ -0,0 +1,27 @@
+#![no_main]
+#![no_std]
+
+use auto_drive_sdk::hardware::the_beginner;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn main() -> () {
+  the_beginner::print(2);
+  the_beginner::delay(50);
+  the_beginner::print(50);
+  the_beginner::set_onboard_led_color(255,0,0);
+  the_beginner::delay(1000);
+  the_beginner::set_onboard_led_color(0,255, 0);
+  the_beginner::delay(2000);
+  the_beginner::print(9999);
+  the_beginner::set_onboard_led_color(255,255, 255);
+  the_beginner::delay(1000);
+  the_beginner::set_onboard_led_color(0,0, 0);
+  the_beginner::delay(2000);
+}
+
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
